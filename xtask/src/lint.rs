2e40afdeafd172aaa7d55669b20d9d7667c3e0f3
Stuart Small <stuart.alan.small@gmail.com>
2022-09-10 20:50:48 -0600

    Add a basic github action for a few minor checks

--- xtask/src/lint.rs
+++ xtask/src/lint.rs
@@ -8,28 +8,14 @@ pub(crate) fn lint() {
 }
 
 fn lint_rust() {
-    println!("Running cargo fmt");
-    let mut fmt_cmd = Command::new("cargo");
-    fmt_cmd.arg("fmt");
-    check_command!(fmt_cmd, "Failed to run cargo fmt: {}");
     //TODO: Run fmt on the excluded crate
     println!("Running clippy");
     let mut lint_cmd = Command::new("cargo");
     lint_cmd.args(&["clippy", "--color=always"]);
     check_command!(lint_cmd, "Failed to run cargo clippy: {}");
-
-    println!("Running cargo spell check");
-    let mut lint_cmd = Command::new("cargo");
-    lint_cmd.args(&["spellcheck", "-m", "1"]);
-    check_command_print_stdout!(lint_cmd, "Failed to run cargo spellcheck: {}");
 }
 
 fn lint_python_sdk() {
-    println!("Running flake8");
-    let mut flake8_cmd = Command::new("flake8");
-    flake8_cmd.args(&["--exclude", "ffi.py", "sdks/python/bowbend/"]);
-    check_command_print_stdout!(flake8_cmd, "flake8 failed: {}");
-
     println!("Running pylint");
     let mut pylint_cmd = Command::new("pylint");
     pylint_cmd.args(&["--rcfile", "sdks/python/.pylintrc", "sdks/python/bowbend/"]);
